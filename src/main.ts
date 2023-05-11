2cfcb9c1359e6ebb41beb3c7ee456632a42160be
CHAO-MUCHACHOS <kirill2006.sm@gmail.com>
2023-05-11 20:24:35 +0300

    342

--- src/main.ts
+++ src/main.ts
@@ -162,3 +162,56 @@ setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 // else (age > 60){
 //     alert('Дед')
 // }
+
+
+
+// Запросить у пользователя число от 0 до 9 и вывести ему 
+// спецсимвол, который расположен на этой клавише (1–!, 
+// 2–@, 3–# и т. д).
+let num = +(prompt("Число")as string)
+switch(num){
+case 1 :
+  console.log("\!");
+break
+
+case 2 :
+  console.log("\@");
+break
+
+case 3 :
+  console.log("\#");
+break
+
+case 4 :
+  console.log("\$");
+break
+
+case 5 :
+  console.log("\%");
+break
+
+case 6 :
+  console.log("\^");
+break
+
+case 7 :
+  console.log("\&");
+break
+
+case 8 :
+  console.log("\*");
+break
+
+case 9 :
+  console.log("\(");
+break
+
+case 0 :
+  console.log("\)");
+break
+
+default:
+  alert('От 0 до 9!')
+  break
+
+}
