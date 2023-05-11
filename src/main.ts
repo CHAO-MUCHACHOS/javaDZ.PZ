52c85da65ad37db15e41cf7c3c41248e49f95496
CHAO-MUCHACHOS <kirill2006.sm@gmail.com>
2023-05-11 20:15:17 +0300

    re

--- src/main.ts
+++ src/main.ts
@@ -142,23 +142,23 @@ setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 // console.log(result)
 
 
-
+// DZ 2 #1
 //Запросить у пользователя его возраст и определить, кем он 
 // является: ребенком (0–2), подростком (12–18), взрослым 
 // (18_60) или пенсионером (60– ...).
 
-let age = +( prompt ("Введите возраст") as string)
+// let age = +( prompt ("Введите возраст") as string)
 
 
-if (age >= 0  || age <= 2){
-alert("ребенок")
-}
-else if (age >= 12  || age <= 18){
-    alert("подростoк")
-}
-else if (age >= 18  || age <= 60){
-    alert("взрослый")
-}
-else (age > 60){
-    alert('Дед')
-}
+// if (age >= 0  || age <= 2){
+// alert("ребенок")
+// }
+// else if (age >= 12  || age <= 18){
+//     alert("подростoк")
+// }
+// else if (age >= 18  || age <= 60){
+//     alert("взрослый")
+// }
+// else (age > 60){
+//     alert('Дед')
+// }
